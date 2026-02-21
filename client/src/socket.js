642cbce49db35e449b5f85de3a79649048bfa392
Kartik Lande <kartiklande70@gmail.com>
2026-02-21 11:21:10 +0530

    cors error fixed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = "http://localhost:5001" || import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
